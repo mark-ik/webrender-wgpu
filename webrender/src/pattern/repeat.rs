72e3eab9924cfe2668c595ddb4cbfeb4ce0324fc
rperta <rperta@mozilla.com>
2026-03-09 12:57:59 +0200

    Revert "Bug 2020923, Bug 2019934 - Make the repetition iterator more NaN-proof. r=gfx-reviewers,lsalzman" for causing wr failures at iframe-and-main-frame-transition-old-main-new-iframe.html
    This reverts commit 069829f552e1d4b5a3854e5a4a7e304b429911b6.
    
    This reverts commit b0037c6c13e7c371d1ff7a8caea979ac75aafc8b.
    
    This reverts commit d7b8eb84e2d01fca6b5957db787e678293caa412.
    
    This reverts commit 12acc76693c30bde512b2a188456e6bcf79b9823.
    
    This reverts commit d5b939096cd694ef9b77c1dd2340757a6d44a7a8.
    
    This reverts commit 7690a76d25995ea13d434dbd6410ed6cfbdccf47.
    
    This reverts commit 303b29fca4b8f02ab541e9ae54df76711bc08113.

--- webrender/src/pattern/repeat.rs
+++ webrender/src/pattern/repeat.rs
@@ -42,9 +42,7 @@ pub fn repeated_pattern(
         ),
         texture_input: PatternTextureInput::new(repeat.src_task_id),
         base_color: ColorF::WHITE,
-        is_opaque: repeat.src_is_opaque
-            && repeat.spacing.width <= 0.0
-            && repeat.spacing.height <= 0.0,
+        is_opaque: repeat.src_is_opaque && repeat.spacing.is_empty(),
     }
 }
 
